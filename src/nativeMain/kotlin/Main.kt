865a473c5556619dbce3dfb697ae7e9d9f6ba071
Julius Vincent Babies <julius@familie-babies.de>
2025-11-15 11:49:36 +0100

    Basic table

--- src/nativeMain/kotlin/Main.kt
+++ src/nativeMain/kotlin/Main.kt
@@ -1,21 +1,119 @@
-import kotlinx.serialization.Serializable
-import kotlinx.serialization.encodeToString
-import kotlinx.serialization.json.Json
+class Cell {
+    var content: String = ""
 
-@Serializable
-private data class Message(
-    val topic: String,
-    val content: String,
-)
+    operator fun String.unaryPlus() {
+        content += this
+    }
+}
 
-private val PrettyPrintJson = Json {
-    prettyPrint = true
+class Row {
+    val cells = mutableListOf<Cell>()
+
+    fun cell(block: Cell.() -> Unit) {
+        val cell = Cell()
+        cell.apply(block)
+        cells.add(cell)
+    }
+
+    override fun toString(): String {
+        return "RowBuilder()"
+    }
+}
+
+class Table {
+
+    private val rows = mutableListOf<Row>()
+    var cellPadding = 1
+
+    fun row(block: Row.() -> Unit) {
+        val row = Row()
+        row.apply(block)
+        rows.add(row)
+    }
+
+    override fun toString(): String {
+        if (rows.isEmpty()) return ""
+        return buildString {
+            rows.forEachIndexed { rowIndex, row ->
+                // Start char
+                when (rowIndex) {
+                    0 -> append("┌")
+                    else -> append("├")
+                }
+
+                // Overline
+                row.cells.forEachIndexed { cellIndex, _ ->
+                    val longestLength = rows.maxOf { it.cells.getOrNull(cellIndex)?.content?.length ?: 0 }
+                    repeat(longestLength + 2*cellPadding) {
+                        append("─")
+                    }
+
+                    if (rowIndex == 0) {
+                        if (cellIndex == row.cells.lastIndex) append("┐")
+                        else append("┬")
+                    } else {
+                        if (cellIndex == row.cells.lastIndex) append("┤")
+                        else append("┼")
+                    }
+                }
+
+                // Content
+                appendLine()
+                append("│")
+                row.cells.forEachIndexed { cellIndex, cell ->
+                    repeat(cellPadding) { append(" ") }
+                    append(cell.content)
+                    val longestLength = rows.maxOf { it.cells.getOrNull(cellIndex)?.content?.length ?: 0 }
+                    repeat(longestLength - cell.content.length + cellPadding) {
+                        append(" ")
+                    }
+
+                    append("│")
+                }
+
+                if (rowIndex < rows.lastIndex) appendLine()
+            }
+
+            // Bottom
+            appendLine()
+            append("└")
+            rows.last().cells.forEachIndexed { cellIndex, _ ->
+                val longestLength = rows.maxOf { it.cells.getOrNull(cellIndex)?.content?.length ?: 0 }
+                repeat(longestLength + 2*cellPadding) {
+                    append("─")
+                }
+
+                if (cellIndex == rows.last().cells.lastIndex) append("┘")
+                else append("┴")
+            }
+        }
+    }
+}
+
+fun buildTable(block: Table.() -> Unit): String {
+    val builder = Table()
+    builder.apply(block)
+    return builder.toString()
 }
 
 fun main() {
-    val message = Message(
-        topic = "Kotlin/Native",
-        content = "Hello!"
-    )
-    println(PrettyPrintJson.encodeToString(message))
+    buildTable {
+        row {
+            cell {
+                +"ID"
+            }
+            cell {
+                +"Name"
+            }
+        }
+
+        row {
+            cell {
+                +"1"
+            }
+            cell {
+                +"Albert"
+            }
+        }
+    }.let(::println)
 }
