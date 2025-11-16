152177589297723963c3870100d793d402c21be4
Julius Vincent Babies <julius@familie-babies.de>
2025-11-16 15:56:46 +0100

    test: Test colspan

--- table-tui/src/nativeTest/kotlin/ColspanTableTest.kt
+++ table-tui/src/nativeTest/kotlin/ColspanTableTest.kt
@@ -1,11 +1,45 @@
 import es.jvbabi.tui.table.buildTable
 import io.kotest.common.ExperimentalKotest
 import io.kotest.core.spec.style.FunSpec
-import io.kotest.engine.test.logging.info
+import io.kotest.matchers.shouldBe
 
 @OptIn(ExperimentalKotest::class)
 class ColspanTableTest : FunSpec({
     test("Center spanned") {
+        val table = buildTable {
+            row {
+                cell {
+                    +"ID"
+                }
+                cell {
+                    +"Name"
+                }
+                cell {
+                    +"Age"
+                }
+                cell {
+                    +"Address"
+                }
+            }
+
+            row {
+                cell { +"1" }
+                cell {
+                    colspan = 2
+                    +"John"
+                }
+                cell { +"20" }
+            }
+        }
+
+        table shouldBe """┌────┬──────┬─────┬─────────┐
+│ ID │ Name │ Age │ Address │
+├────┼──────┴─────┼─────────┤
+│ 1  │ John       │ 20      │
+└────┴────────────┴─────────┘"""
+    }
+
+    test("Last row spanned") {
         val table = buildTable {
             row {
                 cell {
@@ -28,7 +62,10 @@ class ColspanTableTest : FunSpec({
             }
         }
 
-        info { table }
+        table shouldBe """┌────┬──────┬─────┐
+│ ID │ Name │ Age │
+├────┼──────┴─────┤
+│ 1  │ John       │
+└────┴────────────┘"""
     }
-}) {
-}
+})
